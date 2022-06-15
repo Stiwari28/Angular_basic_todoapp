aefb125cc4484d508be75d335da060d42c7052ce
STiwari <71030367+Stiwari28@users.noreply.github.com>
2022-06-15 23:23:22 +0530

    Todos files updated

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,13 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Todo } from '../Todos';
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-
-  constructor() { }
+  todos: Todo[] | undefined ;
+  constructor() {
+    this.todos= [
+      {
+        sno:3,
+        title:"this is title",
+        desc: "Description",
+        active: true
+      },
+      {
+        sno:4,
+        title:"this is title 1",
+        desc: "Description 1",
+        active: true
+      },
+      {
+        sno:5,
+        title:"this is title 2",
+        desc: "Description 2",
+        active: true
+      }
+    ]
+  }
 
   ngOnInit(): void {
   }
