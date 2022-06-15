8d626dc771608bbd46f8fdaf9c669c82e8c91b33
STiwari <71030367+Stiwari28@users.noreply.github.com>
2022-06-15 21:35:48 +0530

    New component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Practice_project1';
+  constructor(){
+    //setTimeout(() => {
+    //  this.title= "Change Title";
+   // }, 2000);
+
+  }
 }
